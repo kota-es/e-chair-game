049df8e8f52915bc3827f1187562e80ff5420604
kota-es <info@k-3110.com>
2025-02-02 21:15:11 +0900

    feat: modify turn result modal layout

--- web/components/modals/TurnResultModal.tsx
+++ web/components/modals/TurnResultModal.tsx
@@ -34,38 +34,61 @@ export default function TurnResultModal({
     ? "電気椅子を回避しました！"
     : "電気椅子を回避されました...";
 
-  const bodyText2 = () => {
-    if (!myRoundStatus || !opponentRoundStatus || !roomData.round)
-      return "結果取得エラー";
-    if (isShocked) {
-      return isAttacker
-        ? `感電回数は${myRoundStatus?.shockedCount}回になりました`
-        : `感電回数は${opponentRoundStatus?.shockedCount}回になりました！`;
-    } else {
-      return isAttacker
-        ? `あなたのスコアは${myRoundStatus?.point}になりました！`
-        : `相手のスコアは${opponentRoundStatus?.point}になりました`;
-    }
-  };
-
   return (
     <dialog
       className="absolute min-w-fit max-w-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  backdrop:bg-black/80 shadow-sm w-full"
       ref={ref}
     >
-      <div className="grid gap-4 backdrop:bg-black/80 p-6 text-card-foreground shadow-sm w-full bg-gray-800 border-2 border-red-500">
-        <div>
+      <div className="grid place-items-center gap-4 backdrop:bg-black/80 p-6 text-card-foreground shadow-sm w-full bg-gray-800 border-2 border-red-500">
+        <div className="flex items-center flex-col gap-4">
           <h2 className="font-semibold text-red-500">
-            <span>{headingText}</span>
+            <span className="text-4xl">{headingText}</span>
           </h2>
-          <p className="pt-1 text-gray-300">{bodyText1}</p>
-          <p className="pt-1 text-gray-300">{bodyText2()}</p>
+          <p className="pt-1 text-2xl font-semibold text-gray-300">
+            {bodyText1}
+          </p>
+          <div className="flex gap-6">
+            <div className="flex flex-col items-center">
+              <div className="text-gray-400">電気椅子</div>
+              <div className="font-bold text-white text-4xl">
+                {roomData?.round?.electricChair}
+              </div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="text-gray-400">座った椅子</div>
+              <div className="font-bold text-white text-4xl">
+                {roomData?.round?.seatedChair}
+              </div>
+            </div>
+          </div>
+          <p className="pt-1 text-xl font-semibold text-gray-300">
+            {roomData?.round?.attackerId === userId ? "あなたの" : "相手の"}
+            スコアが更新されました
+          </p>
+          <div className="flex gap-6">
+            <div className="flex flex-col items-center">
+              <div className="text-gray-400">ポイント</div>
+              <div className="font-bold text-green-400 text-4xl">
+                {roomData?.round?.attackerId === userId
+                  ? myRoundStatus?.point
+                  : opponentRoundStatus?.point}
+              </div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="text-gray-400">感電回数</div>
+              <div className="font-bold text-red-500 text-4xl">
+                {roomData?.round?.attackerId === userId
+                  ? myRoundStatus?.shockedCount
+                  : opponentRoundStatus?.shockedCount}
+              </div>
+            </div>
+          </div>
         </div>
         <button
-          className="inline-flex h-10 justify-center items-center rounded-full bg-red-500 font-bold text-sm text-white"
+          className="inline-flex h-10 w-full justify-center items-center rounded-full bg-red-500 font-bold text-white"
           onClick={close}
         >
-          攻守交代
+          次へ進む
         </button>
       </div>
     </dialog>
