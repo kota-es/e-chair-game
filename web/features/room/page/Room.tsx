d81250332968c4f0b4009d710aad66ba47185561
kota-es <info@k-3110.com>
2025-03-09 16:25:56 +0900

    ref: create chair component

--- web/features/room/page/Room.tsx
+++ web/features/room/page/Room.tsx
@@ -20,42 +20,13 @@ import ActivateShockDialog from "@/features/room/components/dialogs/ActivateShoc
 import NoticeSetDialog from "@/features/room/components/dialogs/NoticeSetDialog";
 import StartTurnDialog from "@/features/room/components/dialogs/StartTurnDialog";
 import useDialog from "@/hooks/useDialog";
+import { Chair } from "@/features/room/components/Chair";
 
 type playerOperation = {
   setElectricShock: boolean;
   selectSitChair: boolean;
   activate: boolean;
   wait: boolean;
-};
-
-const renderChair = (
-  chair: number,
-  setSelectedChair: (chair: number) => void,
-  wait: boolean,
-  selected: boolean
-) => {
-  const index = chair - 1;
-  const angle = ((index - 2) / 12) * 2 * Math.PI;
-  const radius = 45;
-  const left = 50 + radius * Math.cos(angle);
-  const top = 50 + radius * Math.sin(angle);
-
-  const bgColor = selected ? "bg-white" : "bg-gray-700";
-  const textColor = selected ? "text-gray-900" : "text-white";
-  const textFont = selected ? "font-bold" : "font-normal";
-  const textSize = selected ? "text-lg" : "text-sm";
-  const cursor = wait ? "cursor-not-allowed" : "cursor-pointer";
-
-  return (
-    <div
-      key={chair}
-      className={`inline-flex items-center justify-center absolute w-12 h-12 transform -translate-x-1/2 -translate-y-1/2 ${bgColor} ${textColor} ${textFont} ${textSize} transition-all duration-300 border border-white rounded-lg ${cursor} select-none`}
-      style={{ left: `${left}%`, top: `${top}%` }}
-      onClick={wait ? undefined : () => setSelectedChair(chair)}
-    >
-      {chair}
-    </div>
-  );
 };
 
 export default function Room({
@@ -405,14 +376,15 @@ export default function Room({
         </div>
       </div>
       <div className="relative w-full max-w-md aspect-square mx-auto">
-        {roomData?.remainingChairs.map((chair) =>
-          renderChair(
-            chair,
-            setSelectedChair,
-            playerOperation.wait,
-            selectedChair === chair
-          )
-        )}
+        {roomData?.remainingChairs.map((chair) => (
+          <Chair
+            key={chair}
+            chair={chair}
+            setSelectedChair={setSelectedChair}
+            wait={playerOperation.wait}
+            selected={selectedChair === chair}
+          />
+        ))}
         {isAllReady() && (
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
             <p
