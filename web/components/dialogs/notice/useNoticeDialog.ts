c6cb97c5e9607df93a53bc892e1576432fb3148b
kota-es <info@k-3110.com>
2025-04-12 16:55:11 +0900

    ref: add usecallback to modal controll

--- web/components/dialogs/notice/useNoticeDialog.ts
+++ web/components/dialogs/notice/useNoticeDialog.ts
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback } from "react";
 
 export function useNoticeDialog() {
   const dialogRef = useRef<HTMLDialogElement | null>(null);
@@ -11,43 +11,46 @@ export function useNoticeDialog() {
     },
   });
 
-  const showModal = (
-    {
-      title,
-      message,
-      button,
-    }: {
-      title: string;
-      message: string;
-      button: {
-        label: string;
-        action: () => void;
-      };
-    },
-    miliseconds?: number
-  ) => {
-    if (dialogRef.current) {
-      dialogRef.current.showModal();
-      setDialogState({ title, message, button });
-    }
-
-    if (!miliseconds) return;
-
-    if (typeof miliseconds !== "number") {
-      console.error("miliseconds must be a number");
-      return;
-    }
-
-    setTimeout(() => {
-      closeModal();
-    }, miliseconds);
-  };
-
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     if (dialogRef.current) {
       dialogRef.current.close();
     }
-  };
+  }, []);
+
+  const showModal = useCallback(
+    (
+      {
+        title,
+        message,
+        button,
+      }: {
+        title: string;
+        message: string;
+        button: {
+          label: string;
+          action: () => void;
+        };
+      },
+      miliseconds?: number
+    ) => {
+      if (dialogRef.current) {
+        dialogRef.current.showModal();
+        setDialogState({ title, message, button });
+      }
+
+      if (!miliseconds) return;
+
+      if (typeof miliseconds !== "number") {
+        console.error("miliseconds must be a number");
+        return;
+      }
+
+      setTimeout(() => {
+        closeModal();
+      }, miliseconds);
+    },
+    [closeModal]
+  );
 
   return { dialogRef, dialogState, showModal, closeModal };
 }
