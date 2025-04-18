3160fbb09375a19982b77098c773801a20132822
kota-es <info@k-3110.com>
2025-04-18 10:10:11 +0900

    fix: modfiy a get cookie method in middleware

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,11 +1,9 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const cookieStore = await cookies();
-  const userId = cookieStore.get("userId")?.value as string;
-  const roomId = cookieStore.get("roomId")?.value as string;
-  const pathRoomId = request.nextUrl.pathname.split("/").slice(-1)[0];
+  const userId = request.cookies.get("userId")?.value as string;
+  const roomId = request.cookies.get("roomId")?.value as string;
+  const pathRoomId = request.nextUrl.pathname.split("/").pop();
 
   if (!userId || !roomId || roomId !== pathRoomId) {
     return NextResponse.redirect(new URL("/", request.url));
